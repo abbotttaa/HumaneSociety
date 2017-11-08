4d30cb5fed76dfbb9de055405a6b7749baff617b
Taylor Abbott <abbotttaa@gmail.com>
2017-11-08 14:37:29 -0600

    EoD commit and push. Working on logic strings for SQL

---
+++ HumaneSociety/HumaneSociety/CommandSQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class CommandSQL
+    {
+        string choice;
+        string choiceRow;
+        public string CraftSQLRemove()
+        {
+            string SQLRemove = ("DELETE FROM " + choiceRow + " WHERE  " + CustomerName + " = '"+ choice +"'; ")
+        }
+        public string CraftSQLAdd()
+        {
+
+        }
+        public string CraftSQLCheck()
+        {
+            
+        }
+    }
+}
--- HumaneSociety/HumaneSociety/DatabaseManagment.cs
+++ HumaneSociety/HumaneSociety/DatabaseManagment.cs
@@ -10,12 +10,50 @@ namespace HumaneSociety
 {
     class DatabaseManagment
     {
+        public string pet;
         ImportCSV import = new ImportCSV();
         SqlConnection myConnection = new SqlConnection("user id=username;" +
                                        "password=password;server=serverurl;" +
                                        "Trusted_Connection=yes;" +
                                        "database=database; " +
                                        "connection timeout=30");
+        public DatabaseManagment(string pet)
+        {
+            this.pet= pet;
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            SqlCommand myCommand = new SqlCommand();
+        }
+        public void ConnectToDatabase()
+        {
+            pet = "0";
+            SqlCommand myCommand = new SqlCommand(pet, myConnection);
+            myCommand.ExecuteNonQuery();
+        }
+        public void CheckDatabaseForMatches()
+        {
+            try
+            {
+                SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand("select * from Animals", myConnection);
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    Console.WriteLine(myReader["Column1"].ToString());
+                    Console.WriteLine(myReader["Column2"].ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
         public void AddPetToDatabase()
         {
 
@@ -26,9 +64,7 @@ namespace HumaneSociety
         }
         public void ImportCSV()
         {
-            FileStream thisFile = new FileStream("../../inmateTransferList.csv",FileMode.Open);
-            StreamReader sr = new StreamReader(thisFile);
-            
-        }
+            import.ImportNewCsv();
+        }      
     }
 }
